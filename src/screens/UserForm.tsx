e0ac3b32f80207c636e0e1c9cd281cb729d8db76
ayush <dev.ayush.ojha@gmail.com>
2023-08-22 07:49:09 +0530

    added some validations to contact form

--- src/screens/UserForm.tsx
+++ src/screens/UserForm.tsx
@@ -1,18 +1,21 @@
-import * as React from 'react';
-import {View, StyleSheet, Image, Dimensions} from 'react-native';
-import {Button, useTheme, Text} from 'react-native-paper';
+import {View, StyleSheet} from 'react-native';
+import {Button, useTheme, Text, Snackbar} from 'react-native-paper';
 import {useFormik} from 'formik';
 import {AppContainer} from '../components/AppContainer';
 import {TextInput} from '../components/Form/TextInput';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../App';
 import {user} from '../services/apis/User';
-import {IUserListItem} from '../services/interfaces/common';
+import {IUserListItem, TSnackBarProps} from '../services/interfaces/common';
 import {storeData} from '../utils/helperFunctions';
+import {useState} from 'react';
 
 type UserFormProps = NativeStackScreenProps<RootStackParamList, 'userForm'>;
 
 const UserForm = ({navigation, route}: UserFormProps) => {
+  const [snackBar, setSnackBar] = useState<TSnackBarProps>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   // const {width: windowWidth, height: windowHeight} = Dimensions.get('window');
   const theme = useTheme();
   const formik = useFormik({
@@ -31,6 +34,7 @@ const UserForm = ({navigation, route}: UserFormProps) => {
           },
         } as IUserListItem),
     onSubmit: values => {
+      setIsLoading(true);
       if (route.params?.user) {
         user
           .updateContact(values, route.params.token, route.params.user._id)
@@ -39,9 +43,13 @@ const UserForm = ({navigation, route}: UserFormProps) => {
               navigation.pop(),
             );
           })
-          .catch(error => {
-            console.error(error.response.data);
-          });
+          .catch(err => {
+            setSnackBar({
+              message: JSON.stringify('Please fill All require fields'), //err.response.data.error),
+              action: {label: 'Ok', onPress: () => setSnackBar(null)},
+            });
+          })
+          .finally(() => setIsLoading(false));
       } else {
         user
           .addContact(values, route.params.token)
@@ -50,9 +58,13 @@ const UserForm = ({navigation, route}: UserFormProps) => {
               navigation.pop(),
             );
           })
-          .catch(error => {
-            console.error(error.response.data);
-          });
+          .catch(err => {
+            setSnackBar({
+              message: JSON.stringify('Please fill All require fields'), //err.response.data.error),
+              action: {label: 'Ok', onPress: () => setSnackBar(null)},
+            });
+          })
+          .finally(() => setIsLoading(false));
       }
     },
   });
@@ -84,96 +96,109 @@ const UserForm = ({navigation, route}: UserFormProps) => {
   });
 
   return (
-    <AppContainer>
-      <View style={styles.form}>
-        <TextInput
-          fieldName="name"
-          onChange={formik.handleChange('name')}
-          handleBlur={formik.handleBlur}
-          value={formik.values.name}
-          placeholder="Full Name *"
-        />
-        <TextInput
-          fieldName="email"
-          onChange={formik.handleChange('email')}
-          handleBlur={formik.handleBlur}
-          value={formik.values.email}
-          placeholder="Email *"
-        />
-        <TextInput
-          fieldName="phone"
-          onChange={formik.handleChange('phone')}
-          handleBlur={formik.handleBlur}
-          value={formik.values.phone}
-          placeholder="Phone No *"
-        />
+    <>
+      <AppContainer>
+        <View style={styles.form}>
+          <TextInput
+            fieldName="name"
+            onChange={formik.handleChange('name')}
+            handleBlur={formik.handleBlur}
+            value={formik.values.name}
+            placeholder="Full Name *"
+          />
+          <TextInput
+            fieldName="email"
+            onChange={formik.handleChange('email')}
+            handleBlur={formik.handleBlur}
+            value={formik.values.email}
+            placeholder="Email *"
+          />
+          <TextInput
+            fieldName="phone"
+            onChange={formik.handleChange('phone')}
+            handleBlur={formik.handleBlur}
+            value={formik.values.phone}
+            placeholder="Phone No *"
+          />
 
-        <Text variant="titleMedium">Address</Text>
+          <Text variant="titleMedium">Address</Text>
 
-        <TextInput
-          fieldName="addressLine1"
-          onChange={formik.handleChange('address.addressLine1')}
-          handleBlur={formik.handleBlur}
-          value={formik.values.address?.['addressLine1']}
-          placeholder="Address Line 1"
-        />
+          <TextInput
+            fieldName="addressLine1"
+            onChange={formik.handleChange('address.addressLine1')}
+            handleBlur={formik.handleBlur}
+            value={formik.values.address?.['addressLine1']}
+            placeholder="Address Line 1"
+          />
 
-        <TextInput
-          fieldName="addressLine2"
-          onChange={formik.handleChange('address.addressLine2')}
-          handleBlur={formik.handleBlur}
-          value={formik.values.address?.['addressLine2']}
-          placeholder="Address Line 2"
-        />
+          <TextInput
+            fieldName="addressLine2"
+            onChange={formik.handleChange('address.addressLine2')}
+            handleBlur={formik.handleBlur}
+            value={formik.values.address?.['addressLine2']}
+            placeholder="Address Line 2"
+          />
 
-        <TextInput
-          fieldName="city"
-          onChange={formik.handleChange('address.city')}
-          handleBlur={formik.handleBlur}
-          value={formik.values.address?.['city']}
-          placeholder="City"
-        />
+          <TextInput
+            fieldName="city"
+            onChange={formik.handleChange('address.city')}
+            handleBlur={formik.handleBlur}
+            value={formik.values.address?.['city']}
+            placeholder="City"
+          />
 
-        <TextInput
-          fieldName="pinCode"
-          onChange={formik.handleChange('address.pinCode')}
-          handleBlur={formik.handleBlur}
-          value={formik.values.address?.['pinCode'] || undefined}
-          placeholder="pinCode"
-        />
+          <TextInput
+            fieldName="pinCode"
+            onChange={formik.handleChange('address.pinCode')}
+            handleBlur={formik.handleBlur}
+            value={formik.values.address?.['pinCode'] || undefined}
+            placeholder="pinCode"
+          />
 
-        <TextInput
-          fieldName="state"
-          onChange={formik.handleChange('address.state')}
-          handleBlur={formik.handleBlur}
-          value={formik.values.address?.['state']}
-          placeholder="State"
-        />
+          <TextInput
+            fieldName="state"
+            onChange={formik.handleChange('address.state')}
+            handleBlur={formik.handleBlur}
+            value={formik.values.address?.['state']}
+            placeholder="State"
+          />
 
-        <View style={styles.buttonContainer}>
-          <Button
-            mode="outlined"
-            style={styles.cancelButton}
-            onPress={() => {
-              navigation.pop();
-            }}
-            labelStyle={{fontSize: 20}}
-            contentStyle={{padding: 3}}>
-            Cancel
-          </Button>
-          <Button
-            mode="contained"
-            style={styles.saveButton}
-            onPress={() => {
-              formik.handleSubmit();
-            }}
-            labelStyle={{fontSize: 20}}
-            contentStyle={{padding: 3}}>
-            Save
-          </Button>
+          <View style={styles.buttonContainer}>
+            <Button
+              mode="outlined"
+              style={styles.cancelButton}
+              onPress={() => {
+                navigation.pop();
+              }}
+              labelStyle={{fontSize: 20}}
+              contentStyle={{padding: 3}}>
+              Cancel
+            </Button>
+            <Button
+              mode="contained"
+              loading={isLoading}
+              style={styles.saveButton}
+              onPress={() => {
+                formik.handleSubmit();
+              }}
+              labelStyle={{fontSize: 20}}
+              contentStyle={{padding: 3}}>
+              Save
+            </Button>
+          </View>
         </View>
-      </View>
-    </AppContainer>
+      </AppContainer>
+
+      <Snackbar
+        visible={snackBar != null}
+        onDismiss={() => {
+          setSnackBar(null);
+        }}
+        duration={snackBar?.duration}
+        action={snackBar?.action}>
+        {snackBar?.message}
+      </Snackbar>
+    </>
   );
 };
 
